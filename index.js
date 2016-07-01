5ca65af573f6fb22cef6ede2bb7b2d78af63cfc6
Teacher <Teacher>
2016-07-01 19:36:59 -0400

    set up dependencies in database

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var lowdb = require('lowdb');
 var server = express();
+
 var port = process.env.PORT || 8080;
+var db = lowdb('db.json');
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 server.get('/todos', function (request, response){
   response.send('GET todos');
