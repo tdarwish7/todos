1e55d503eca9cc086f3c4a54ad3efc4adb250f90
Teacher <Teacher>
2016-07-01 19:31:30 -0400

    restored routes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var lowdb = require('lowdb');
 var server = express();
+
 var port = process.env.PORT || 8080;
 
 server.get('/todos', function (request, response){
