9cd19bd4aab8217a98f39b36d9640c2f9aa128df
Teacher <Teacher>
2016-07-01 19:14:52 -0400

    set up routes

--- index.js
+++ index.js
@@ -2,5 +2,25 @@ var express = require('express');
 var server = express();
 var port = process.env.PORT || 8080;
 
+server.get('/todos', function (request, response){
+  response.send('GET todos');
+});
+
+server.get('/todos/:id', function (request, response){
+  response.send('GET todos :id');
+});
+
+server.post('/todos', function (request, response){
+  response.send('POST todos');
+});
+
+server.put('/todos/:id', function (request, response){
+  response.send('PUT todos :id');
+});
+
+server.delete('/todos/:id', function (request, response){
+  response.send('DELETE todos :id');
+});
+
 
 server.listen(port);
