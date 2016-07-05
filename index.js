2057d45116c43bafb3ee0654d49d2a85d066ff80
Teacher <Teacher>
2016-07-05 19:07:52 -0400

    fixed merge conflicts

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var lowdb = require('lowdb');
 var server = express();
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> setupDatabase
 var uuid = require('uuid');
->>>>>>> setupDatabase
 
 var port = process.env.PORT || 8080;
 var db = lowdb('db.json');
@@ -66,26 +59,5 @@ server.delete('/todos/:id', function (request, response){
             .value();
   response.send(todo);
 });
-
-server.get('/todos', function (request, response){
-  response.send('GET todos');
-});
-
-server.get('/todos/:id', function (request, response){
-  response.send('GET todos :id');
-});
-
-server.post('/todos', function (request, response){
-  response.send('POST todos');
-});
-
-server.put('/todos/:id', function (request, response){
-  response.send('PUT todos :id');
-});
-
-server.delete('/todos/:id', function (request, response){
-  response.send('DELETE todos :id');
-});
-
 
 server.listen(port);
